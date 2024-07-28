8a0df466b2f8122c35a12dbabacd3cf577c272d5
JuanSilva01 <juan.silva@bandtec.com.br>
2024-07-28 13:52:21 -0300

    Alteração API

--- code/no-copy/src/api.js
+++ code/no-copy/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiLogin = axios.create({
-    baseURL : `http://127.0.0.1:5000/`//http://52.0.52.140:8080
+    baseURL : `http://50.16.152.183:5000/`//http://52.0.52.140:8080
 })
 
 export default apiLogin;
