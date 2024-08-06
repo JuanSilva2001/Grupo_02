7350a15e9fb5d28188b1e1396dfa2f4ad8029be9
JuanSilva01 <juan.silva@bandtec.com.br>
2024-08-06 19:21:14 -0300

    Testando ip da API

--- code/no-copy/src/api.js
+++ code/no-copy/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiLogin = axios.create({
-    baseURL : `http://localhost:5000/`//http://52.0.52.140:8080
+    baseURL : `http://127.0.0.1:5000/`//http://52.0.52.140:8080
 })
 
 export default apiLogin;
