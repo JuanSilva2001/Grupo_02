f2b5f9f1ead007583b8adfea4e36c95f19d19910
JuanSilva01 <juan.silva@bandtec.com.br>
2024-08-28 15:20:36 -0300

    subindo site

--- code/no-copy/src/api.js
+++ code/no-copy/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiLogin = axios.create({
-    baseURL : `http://127.0.0.1:5000/`//http://52.0.52.140:8080
+    baseURL : `https://f6du96p34c.execute-api.us-east-1.amazonaws.com/v1`//http://52.0.52.140:8080
 })
 
 export default apiLogin;
--- code/no-copy/src/paginas/avaliarMusica/AvaliarMusica.js
+++ code/no-copy/src/paginas/avaliarMusica/AvaliarMusica.js
@@ -7,19 +7,41 @@ import BotaoVerdeSelva from '../../componentes/botoes/BotaoVerdeSelva';
 import api from '../../api'
 
 const AvaliarMusica = () => {
-  const [file, setFile] = useState(null);
-  async function enviar(e) {
-    setFile(document.getElementById('Arquivo').files[0]);
-    const formData = new FormData();
-    formData.append('file', file);
-    var codigo = sessionStorage.getItem('Codigo')
-    console.log(codigo)
-    e.preventDefault();
-    api.post(`/upload/${codigo}`, formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data'
-      },
-    });
+  async function enviar() {
+        alert('Funcionando')
+        const fileInput = document.getElementById('audioFile');
+        const file = fileInput.files[0];
+        const reader = new FileReader();
+        reader.onload = async (e) => {
+            const arrayBuffer = e.target.result;
+            try {
+                const response = await fetch('https://f6du96p34c.execute-api.us-east-1.amazonaws.com/v1/send-songs', {
+                    method: 'POST',
+                    body: arrayBuffer,
+                    headers: {
+                        'Content-Type': 'audio/mpeg', // Ensure this matches the expected MIME type
+                    },
+                });
+                alert(response)
+
+                const result = await response.text(); // Get the response text
+
+                if (response.ok) {
+                    alert('Áudio enviado com sucesso!');
+                } else {
+                    alert(`Falha ao enviar o áudio. Código: ${response.status}. Mensagem: ${result}`);
+                }
+            } catch (error) {
+                console.error('Erro ao enviar o áudio:', error); // Log the error
+                alert(`Ocorreu um erro ao enviar o áudio: ${error.message}`);
+            }
+        };
+
+        reader.onerror = (error) => {
+            console.error('Erro ao ler o arquivo:', error); // Log the error
+            console.log(`Erro ao ler o arquivo: ${error.message}`);
+        };
+        reader.readAsArrayBuffer(file);
 }
   return (
     <div className="avaliar-musica">
