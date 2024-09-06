b4be6be753303bc703a74d4a8d7f0075c6ce8222
franciscone <mathfranciscone@gmail.com>
2024-09-06 11:42:12 -0300

    New eatures

--- code/bot-baixamusica/bot.py
+++ code/bot-baixamusica/bot.py
@@ -40,56 +40,37 @@ def get_video_urls(api_key, search_term, max_results=50):
     
     return video_urls
 
-# Exemplo de uso
-api_key = 'AIzaSyCWObnv4njvm1AjwgzZUzh87RKTUW6D2CQ'  # Substitua pela sua chave de API
+api_key = 'AIzaSyCWObnv4njvm1AjwgzZUzh87RKTUW6D2CQ'
 search_term = input("Prompt: ")
-urls = get_video_urls(api_key, search_term, max_results=5000)  # Obtendo até 10 mil URLs
+urls = get_video_urls(api_key, search_term, max_results=5000)
 
-# Exibindo as URLs
 for url in urls:
     df_urls.append(url)
 
-# Configuração do cliente S3 usando credenciais locais
 s3_client = boto3.client('s3')
 sts = boto3.client('sts')
 sts.get_caller_identity()
 
 def baixar_e_enviar_para_s3(url, bucket_name):
     try:
-        # Cria um objeto YouTube a partir da URL
         video = YouTube(url).streams.filter(only_audio=True).first()
-
-        # URL do stream de áudio
         audio_url = video.url
-
-        # Nome do arquivo no S3
         file_name = f"{video.title}.wav"
 
-        # Faz o download do áudio e converte para WAV
         response = requests.get(audio_url, stream=True)
-        response.raise_for_status()  # Lança um erro para status de resposta inválido
-        
-        # Cria um buffer de memória
+        response.raise_for_status() 
         buffer = io.BytesIO()
-        
-        # Salva os dados no buffer em chunks
+
         for chunk in response.iter_content(chunk_size=8192):
             buffer.write(chunk)
 
-        # Reinicia o buffer para leitura do início
         buffer.seek(0)
+        audio_segment = AudioSegment.from_file(buffer, format="mp4")
 
-        # Converte o buffer para AudioSegment
-        audio_segment = AudioSegment.from_file(buffer, format="mp4")  # `mp4` é usado para o áudio no YouTube
-
-        # Cria um buffer para o arquivo WAV
         wav_buffer = io.BytesIO()
         audio_segment.export(wav_buffer, format="wav")
-
-        # Reinicia o buffer para leitura do início
         wav_buffer.seek(0)
 
-        # Faz o upload do buffer WAV para o S3
         s3_client.upload_fileobj(wav_buffer, bucket_name, file_name, ExtraArgs={'ContentType': 'audio/wav'})
 
         print(f"{video.title} baixado, convertido para WAV e enviado para o bucket S3 com sucesso.")
@@ -103,7 +84,6 @@ def baixar_e_enviar_para_s3(url, bucket_name):
     except Exception as e:
         print(f"Erro ao processar {url}: {e}")
 
-# Processar cada URL
-bucket_name = 'musics-raw'  # Substitua pelo nome do seu bucket S3
+bucket_name = 'musics-raw'
 for url in df_urls:
     baixar_e_enviar_para_s3(url, bucket_name)
